ce18c833464caf1599e05460463b4edaca2479b1
Olytech <adams.cosmas@gmail.com>
2021-08-04 05:04:32 -0700

    dashboards updated

---
+++ resources/views/partners/property.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master-layouts')
+
+@section('title') @lang('translation.Dashboard') @endsection
+
+@section('css')
+    <link href="{{ URL::asset('/assets/libs/chartist/chartist.min.css') }}" rel="stylesheet" type="text/css" />
+@endsection
+
+@section('content')
+
+
+
+
+
+@endsection
+
+@section('script')
+    <!-- Peity chart-->
+    <script src="{{ URL::asset('/assets/libs/peity/peity.min.js') }}"></script>
+
+    <!-- Plugin Js-->
+    <script src="{{ URL::asset('/assets/libs/chartist/chartist.min.js') }}"></script>
+
+    <script src="{{ URL::asset('/assets/js/pages/dashboard.init.js') }}"></script>
+@endsection
