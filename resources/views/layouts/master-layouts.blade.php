cdbb01164dba9c8a7497178df52e6cde10b035ac
Olytech <adams.cosmas@gmail.com>
2021-08-04 06:32:52 -0700

    dashboards dealers Updated

--- resources/views/layouts/master-layouts.blade.php
+++ resources/views/layouts/master-layouts.blade.php
@@ -51,11 +51,11 @@
     <!-- END layout-wrapper -->
 
     <!-- Right Sidebar -->
-    @include('layouts.right-sidebar')
+    <!-- @include('layouts.right-sidebar') -->
     <!-- /Right-bar -->
 
     <!-- Right bar overlay-->
-    <div class="rightbar-overlay"></div>
+    <!-- <div class="rightbar-overlay"></div> -->
 
     <!-- JAVASCRIPT -->
     @include('layouts.footer-script')
